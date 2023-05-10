94b03e5d0938d46dd642bf0264c906d4f1b548ef
Sullivown <daniel.w.t.sullivan@gmail.com>
2023-05-10 12:17:34 +0100

    Add Navbar and user session logic

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,30 @@
+'use client';
+
 import './globals.css';
-import { Inter } from 'next/font/google';
 import Provider from './provider';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata = {
-	title: 'BG Play Log',
-	description: 'Log your board game plays!',
-};
+import Navigation from './components/Navigation';
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang='en'>
-			<body className={inter.className}>
-				<Provider>{children}</Provider>
+		<html lang='en' className='h-full bg-gray-100'>
+			<body className='h-full'>
+				<Provider>
+					<div className='min-h-full'>
+						<Navigation />
+						<header className='bg-white shadow'>
+							<div className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
+								<h1 className='text-3xl font-bold tracking-tight text-gray-900'>
+									Dashboard
+								</h1>
+							</div>
+						</header>
+						<main>
+							<div className='mx-auto max-w-7xl py-6 sm:px-6 lg:px-8'>
+								{children}
+							</div>
+						</main>
+					</div>
+				</Provider>
 			</body>
 		</html>
 	);
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,7 @@
-import AccountControls from './components/AccountControls';
-
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
 			<h1>Hello!</h1>
-			<AccountControls />
 		</main>
 	);
 }
