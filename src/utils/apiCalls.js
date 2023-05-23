1e7278a23b695108a52ab1717c3fc01732dea3e6
Sullivown <daniel.w.t.sullivan@gmail.com>
2023-05-23 11:50:19 +0100

    Update API calls to use contentfulClient library

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,15 +1,14 @@
-const spaceId = process.env.CONTENTFUL_SPACE_ID;
-const accessToken = process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN;
+const contentful = require('contentful');
+const client = contentful.createClient({
+	space: process.env.CONTENTFUL_SPACE_ID,
+	accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN,
+	environment: 'master',
+});
 
-export async function getLocationsForUser(user) {
-	const res = await fetch(
-		`https://cdn.contentful.com/spaces/${spaceId}/entries?access_token=${accessToken}&content_type=location&fields.user=${user}`
-	);
+export function getLocationsForUser(user) {
+	const data = client
+		.getEntries({ 'fields.user': user, content_type: 'location' })
+		.catch(console.error);
 
-	// Handle errors
-	if (!res.ok) {
-		throw new Error('Failed to fetch data');
-	}
-
-	return res.json();
+	return data;
 }
