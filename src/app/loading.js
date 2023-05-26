f848c9a675552e39103e67e3d9ff283132137900
Sullivown <daniel.w.t.sullivan@gmail.com>
2023-05-26 11:41:27 +0100

    Add loading top level loading component

---
+++ src/app/loading.js
@@ -0,0 +1,8 @@
+export default function Loading() {
+	// You can add any UI inside Loading, including a Skeleton.
+	return (
+		<div className='flex flex-col items-center mx-auto max-w-7xl p-4 sm:p-6 lg:p-8'>
+			<h1>Loading...</h1>
+		</div>
+	);
+}
